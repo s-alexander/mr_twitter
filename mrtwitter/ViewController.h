f9f610f2e992e1f3efa07f3f8c55541d8f9b592d
Александр Сергеев <funny.is2k1@gmail.com>
2012-09-20 14:22:07 +0400

    Empty project created

---
+++ mrtwitter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  mrtwitter
+//
+//  Created by Александр Сергеев on 9/20/12.
+//  Copyright (c) 2012 Alexander Sergeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ mrtwitter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  mrtwitter
+//
+//  Created by Александр Сергеев on 9/20/12.
+//  Copyright (c) 2012 Alexander Sergeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
